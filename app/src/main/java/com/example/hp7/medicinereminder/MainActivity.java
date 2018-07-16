7a991f1bbd260f525c227474fb43aa493b927551
sanyamsingla004 <sanyamsingla004@gmail.com>
2018-07-16 16:28:56 +0530

    first commit

---
+++ app/src/main/java/com/example/hp7/medicinereminder/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hp7.medicinereminder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
